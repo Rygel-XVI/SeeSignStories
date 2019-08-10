70a988d0a6f633bd58d80b8a98878a3c7f96db67
Rygel-XVI <micha232@umn.edu>
2019-08-09 18:00:21 -0700

    added react-youtube

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import YouTube from 'react-youtube';
 
 
 class Main extends Component {
@@ -10,12 +11,15 @@ class Main extends Component {
     }
   }
 
+  displayVideo() {
+    return this.state.videos.map(v => <YouTube videoId={v.id.videoId} key={v.id.videoId} />
+)}
+
   componentDidMount() {
       // all of the user stats
       // fetch(`https://www.googleapis.com/youtube/v3/channels?part=snippet%2CcontentDetails%2Cstatistics&id=UCzm23K3jWedsp9SQejnOrpA&key=${process.env.REACT_APP_KEY}`)
 
       fetch(`https://www.googleapis.com/youtube/v3/search?key=${process.env.REACT_APP_KEY}&channelId=${process.env.REACT_APP_CHANNEL_ID}&part=snippet,id&type=video`)
-
       .then(resp => resp.json())
       .then((json) => {
       this.setState({ videos: json.items })
@@ -25,7 +29,7 @@ class Main extends Component {
   render() {
     return (
       <div className="Main">
-
+        {this.displayVideo()}
       </div>
     )}
 }
