9eb89f7d7d9d6ed16233c558a1390e690013462d
Rygel-XVI <micha232@umn.edu>
2019-08-16 13:07:36 -0700

    changed a few things so it works better between browsers

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -3,9 +3,7 @@ import asl_read from '../ASL_Read.jpg'
 
 const Banner = ({props}) => (
   <div className="banner">
-  <div className="logo">
-    <img className="logo-img" src={asl_read} alt="Picture of ASL sign for read"/>
-  </ div>
+  <img className="logo-img" src={asl_read} alt="ASL sign for read"/>
   <div className="banner-text">
     <h1>See Sign Stories</h1>
   </ div>
