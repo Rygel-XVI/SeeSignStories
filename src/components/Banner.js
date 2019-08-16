223d57a05b20120d970d84e2d0a9eaece2d8143a
Rygel-XVI <micha232@umn.edu>
2019-08-16 11:46:39 -0700

    edited banner format please work on heroku

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -6,7 +6,9 @@ const Banner = ({props}) => (
   <div className="logo">
     <img className="logo-img" src={asl_read} alt="Picture of ASL sign for read"/>
   </ div>
-  <h1 className="banner-text">See Sign Stories</h1>
+  <div className="banner-text">
+    <h1>See Sign Stories</h1>
+  </ div>
   <br />
   </div>
 )
