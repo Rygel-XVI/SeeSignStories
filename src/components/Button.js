6d99646cd516380bdf7c5c088fdc0cb85948c79e
Rygel-XVI <micha232@umn.edu>
2019-08-19 18:43:23 -0700

    refactored buttons into reusable presenational component

---
+++ src/components/Button.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+const Button = ({klass, text}) => (
+  <button className={klass} >{text}</ button>
+)
+
+export default Button
--- src/containers/ButtonNav.js
+++ src/containers/ButtonNav.js
@@ -1,21 +1,29 @@
 import React, { Component } from 'react';
-import { AwesomeButton } from "react-awesome-button";
-import "react-awesome-button/dist/styles.css";
+import Button from '../components/Button'
 
 class ButtonNav extends Component {
 
   render() {
-
     return (
       <div className="buttons-nav">
-      <button className="buttons-1 red" >Genre</ button>
-      <button className="buttons-1 orange" >Grade</ button>
-      <button className="buttons-1 green" >AR Level</ button>
+      <Button text="Genre" klass="buttons-1 red" />
+      <Button text="Grade" klass="buttons-1 orange" />
+      <Button text="AR Level" klass="buttons-1 green" />
       </ div>
 
     )
+
   }
 
 }
 
 export default ButtonNav;
+
+// return (
+//   <div className="buttons-nav">
+//   <button className="buttons-1 red" >Genre</ button>
+//   <button className="buttons-1 orange" >Grade</ button>
+//   <button className="buttons-1 green" >AR Level</ button>
+//   </ div>
+//
+// )
