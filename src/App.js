d7736483843476726bee234ffb8f46bc166345c3
Rygel-XVI <micha232@umn.edu>
2019-08-20 19:45:04 -0700

    escalated fetch requests to App and moved logic out of Main. Implemented initial route

--- src/App.js
+++ src/App.js
@@ -1,20 +1,64 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
 import routes from './routes'
 import Main from './containers/Main'
 import Banner from './components/Banner'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 
-function App() {
-  return (
-    <div className="App">
-      <Banner />
-      <hr border="50px" color='black'/>
-      <br />
-      {routes}
-    </div>
-  );
-}
 
-export default App;
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      videos: [],
+      opts: {
+        height: '100%',
+        width: '100%',
+      }
+    }
+  }
+
+  fetchVideoIds() {
+    let vId = []
+    fetch(`https://www.googleapis.com/youtube/v3/search?key=${process.env.REACT_APP_KEY}&channelId=${process.env.REACT_APP_CHANNEL_ID}&part=snippet,id&type=video`)
+      .then(resp => resp.json())
+      .then((json) => {
+        vId = json.items.map(v => v.id.videoId).join(',')
+        this.fetchVideoTags(vId)
+      })
+    }
+
+    fetchVideoTags(videoIdString) {
+      fetch(`https://www.googleapis.com/youtube/v3/videos?part=snippet&id=${videoIdString}&key=${process.env.REACT_APP_KEY}`)
+        .then(resp => resp.json())
+        .then((json) => {
+          this.setState({
+            videos: json.items
+          })
+        })
+      }
+
+      componentDidMount() {
+        this.fetchVideoIds()
+      }
+
+      render() {
+
+        return (
+          <div className="App">
+
+          <Banner />
+          <hr border="50px" color='black'/>
+          <br />
+          <Router>
+          <Switch id='routes'>
+          <Route exact path="/" render={() => <Main videos={this.state.videos} />} />
+          </Switch>
+          </ Router>
+          </ div>
+        )}
+      }
+
+      export default App;
--- src/containers/Main.js
+++ src/containers/Main.js
@@ -4,57 +4,25 @@ import VideoCard from '../components/VideoCard';
 
 
 class Main extends Component {
-
   constructor(props) {
     super(props)
     this.state = {
-      videos: [],
-      storyOfWeek: 'No Story of the Week',
-      opts: {
-        height: '100%',
-        width: '100%',
-      }
+      sow: "No Story of the Week"
     }
   }
 
-  displayVideo() {
-    return this.state.videos.map(v => <VideoCard key={v.id} id={v.id} tags={v.snippet.tags}  title={v.snippet.title} />
-  )}
+  // displayVideo() {
+  //   return this.state.videos.map(v => <VideoCard key={v.id} id={v.id} tags={v.snippet.tags}  title={v.snippet.title} />
+  // )}
 
   displaySOW() {
-    let sow = this.state.storyOfWeek
+    let sow = this.props.videos.find(v => v.snippet.tags.includes("Folk Tale")) || "No Story of the Week"
 
     if (sow === "No Story of the Week") {
       return sow
     } else {
       return <VideoCard key={sow.id} id={sow.id} tags={sow.snippet.tags}  title={sow.snippet.title} opts={this.state.opts}/>
     }
-  }
-
-  fetchVideoIds() {
-    let vId = []
-    fetch(`https://www.googleapis.com/youtube/v3/search?key=${process.env.REACT_APP_KEY}&channelId=${process.env.REACT_APP_CHANNEL_ID}&part=snippet,id&type=video`)
-      .then(resp => resp.json())
-      .then((json) => {
-        vId = json.items.map(v => v.id.videoId).join(',')
-        this.fetchVideoTags(vId)
-    })
-  }
-
-  fetchVideoTags(videoIdString) {
-    fetch(`https://www.googleapis.com/youtube/v3/videos?part=snippet&id=${videoIdString}&key=${process.env.REACT_APP_KEY}`)
-      .then(resp => resp.json())
-      .then((json) => {
-        let storyOfWeek = json.items.find(v => v.snippet.tags.includes("Folk Tale")) || "No Story of the Week"
-        this.setState({
-          videos: json.items,
-          storyOfWeek: storyOfWeek
-        })
-    })
-  }
-
-  componentDidMount() {
-    this.fetchVideoIds()
   }
 
   render() {
