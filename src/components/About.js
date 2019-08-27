b043013b9d1d07b484e752cc5ea2b0de2cc1ade6
Rygel-XVI <micha232@umn.edu>
2019-08-27 12:24:28 -0700

    added about text and minor styling

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,32 @@ import React from 'react'
 const About = ({props}) => (
   <div className='about'>
   <h1>About the site</h1>
-  <p>in progress</p>
+  <p>
+
+  As a teacher I know how important it is that all students read and be read to. This is how they develop the fluency and vocabulary to improve their literacy skills.
+
+  </p>
+
+<p>
+This project started when I realized that my deaf and hard-of-hearing students do not have the same access to literacy that hearing students have.
+</p>
+
+<p>
+The language used in the high-quality literature that students’ have access to in the classroom can be challenging for both the students and their parents. Frequently the vocabulary used in stories is not the same vocabulary we use when communicating with each other. This can make the read-aloud process laborious for the child and their parent, especially, if this means looking up new signs frequently when reading the text together.
+</p>
+
+<p>
+I wanted to provide an option for students to be read to fluently in their modality of communication. The students can read along with the video, they can watch the videos to improve their comprehension of the story and the whole family can be presented with a model to learn new sign vocabulary that they may not have se  </p>
+
+<p>en before.
+
+I want the families in my class, and beyond, to have a resource for a read-aloud for their children.
+</p>
+
+<p>
+This website is a work in progress and a labor of love. In the future there will be more stories at more grade levels. Currently, all of the stories are in Signing Exact English and spoken English, I hope to add stories in signed in American Sign Language in the future.
+
+</p>
   </div>
 
 )
