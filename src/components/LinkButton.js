11367643fe0a9ca48b0ada3fa76411056cb58cf7
Rygel-XVI <micha232@umn.edu>
2019-08-29 12:35:31 -0700

    nested button inside of a <Link /> component so it shows the route in the browser and keeps the styling of the button with the whole button clickable

--- src/components/LinkButton.js
+++ src/components/LinkButton.js
@@ -1,10 +1,11 @@
+
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom'
 
 const LinkButton = ({klass, text, path, history}) => (
-  <button className={klass}>
-  <Link to={path}>{text}</Link>
-  </button>
+  <Link to={path}>
+  <button className={klass}>{text}</button>
+  </Link>
 )
 
 export default LinkButton
