9868e73b993f01cda79d1c8c796c6b1218c3ea5a
Rygel-XVI <micha232@umn.edu>
2020-03-22 21:37:55 -0700

    redid logic to work with database to reduce api calls

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -12,15 +12,20 @@ class Main extends Component {
 
 
   displaySOW() {
-    let sow = this.props.videos.find(v => v.snippet.tags.includes("Story of the Week"))
-    return sow !== undefined ? <StoryOfWeek sow={sow} /> : "No Story of the Week"
+    // let sow = this.props.videos.find(v => v.snippet.tags.includes("Story of the Week"))
+    // return sow !== undefined ? <StoryOfWeek sow={sow} /> : "No Story of the Week"
+  }
+  
+  displayTeachermMessage() {
+    let message = "Welcome to the website. Through this website you can find storybooks that are spoken aloud and signed. Please read along. This website is still in early stage development if it is unaccessible you can find the videos on Youtube."
+    return 
   }
 
   render() {
 
     return (
       <div className="main videos">
-        {this.displaySOW()}
+      <h2>Welcome to the website. Through this website you can find storybooks that are spoken aloud and signed. Please read along. This website is still in early stage development if it is unaccessible you can find the videos on Youtube.</h2>
       </div>
     )}
 }
